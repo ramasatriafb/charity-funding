e9584a9bb40a22b7614b5b3c2a9052e966097fec
Rama Firmansyah <ramasatriafb@gmail.com>
2021-06-09 15:41:12 +0700

    Update Component

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports ={
+    charities : [
+        {
+            id: 1,
+            title: 'First Charity',
+            categoru: 'Social',
+            thumbnail: 'https://live.staticflickr.com/3054/2282693104_e118f5d980_n.jpg',
+            pledged: 0,
+            target: 1000000,
+            date_end: +new Date('8 June 2020'),
+            profile_photo: 'https://live.staticflickr.com/3054/2282693104_e118f5d980_n.jpg',
+            profile_name: 'Vieow',
+            no_pledges: 0
+        }
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+    import CharityList from "..components/CharityList.svelte";
+    import Header from "..components/Header.svelte";
+    import Welcome from "..components/Welcome.svelte";
+    import Promo from "..components/Promo.svelte";
+    import Footer from "..components/Footer.svelte";
+</script>
+
+<Header />
+<Welcome />
+    <CharityList />
+    <Promo />
+    <Footer />
